75b767808d5034239c981ac246dcb81dd2c28748
mikroman <65836036+mikroman@users.noreply.github.com>
2021-11-17 06:41:49 -0800

    17.1

--- PLANET2.asm
+++ PLANET2.asm
@@ -4624,6 +4624,8 @@ SurfQuad:
 	.byte $19,$50,$40,$14,$54,$55,$55
 ImgLaser:
 	.byte $95//unused
+//Laser bam sprite data (tail to head)
+//colours: $00 bb, $10 bF, $20 Fb, $30 FF
 	.byte $00,$00,$30,$00,$20,$30,$10,$30
 	.byte $00,$30,$30,$20,$30,$30,$00,$30
 	.byte $30,$30,$30,$30,$30,$30,$30,$30
@@ -4697,21 +4699,33 @@ Duration:
 	.byte $1D,$1D,$1D,$1E
 
 SpriteLen:
-//$2D00,X	Sprite data lengths
+//$2D00,X	Sprite data lengths for :
+//		SHIP LAND MUT BAIT BOMB SWAR MAN POD
 	.byte $30,$20,$20,$14,$18,$0C,$08,$18
 	.byte $02,$28,$28
+//		KUGL S250 S500
 SpriteV_l:
 //$2D0B,X	LB of vectors to sprite data
-	.byte $C0,$2C,$4C,$6C,$94,$AC,$A0,$A8
-	.byte $B8,$BE,$CE
+	// .byte $C0,$2C,$4C,$6C,$94,$AC,$A0,$A8
+	// .byte $B8,$BE,$CE
+.byte	<imgShipR,<imgLander,<imgMutant,<imgBaiter
+.byte	<imgBomber,<imgSwarmer,<imgMan,<imgPod
+.byte	<imgKugel,<img250,<img500
 SpriteV_h:
 //$2D16,X	HB of vectors to sprite data
-	.byte $0F,$10,$10,$10,$10,$10,$0F,$0F
-	.byte $10,$10,$10
+	// .byte $0F,$10,$10,$10,$10,$10,$0F,$0F
+	// .byte $10,$10,$10
+.byte	>imgShipR,>imgLander,>imgMutant,>imgBaiter
+.byte	>imgBomber,>imgSwarmer,>imgMan,>imgPod
+.byte	>imgKugel,>img250,>img500	
+
 imgDot:
 //$2D21,X	Minimap dots (2x2px) for each sprite #
+//		  SHIP    LAND    MUT     BAIT
 	.byte $FF,$FF,$8A,$88,$A2,$88,$88,$88
+//		  BOMB    SWAR    MAN     POD
 	.byte $A2,$A2,$82,$8A,$A8,$A8,$20,$20
+//		  KUGL    S250    S500	
 	.byte $00,$D8,$00,$00,$00,$FF
 //imgDotR:
 //2D37		imgDot[] dots, >> 1 pixel to the right
@@ -4720,20 +4734,26 @@ imgDot:
 	.byte $00,$6C,$00,$00,$00,$7F
 SpriteMaxY:
 //$2D4D		Sprite height bitmask (-> _heightmask)
+//		SHIP LAND MUT BAIT BOMB SWAR MAN POD
 	.byte $07,$07,$07,$03,$07,$03,$0F,$07
 	.byte $03,$07,$07
+//		KUGL S250 S500
 Points_l:
-//$2D58,X	bcd_t[11] unit point scores (x1)
+//$2D58,X	bcd_t unit point scores (x1)
 	.byte $00,$50,$50,$50,$00,$50,$00,$00
 	.byte $25,$00,$00
 Points_h:
-//$2D63,X	bcd_t[11] unit point scores (x100)
+//$2D63,X	bcd_t unit point scores (x100)
+//		SHIP LAND  MUT BAIT BOMB SWAR MAN POD
 	.byte $00,$01,$01,$01,$02,$02,$00,$10
 	.byte $00,$00,$00
+//		KUGL S250 S500
 DoWarp:
-//$2D6E,Y	bool[11] unit 'warp in' animation
+//$2D6E,Y	bool unit 'warp in' animation
+//		SHIP LAND  MUT BAIT BOMB SWAR MAN POD
 	.byte $00,$01,$01,$01,$01,$00,$00,$01
 	.byte $00,$00,$00
+//		KUGL S250 S500
 //$2d79		135 unused bytes
 	.byte $BB,$CA,$BA,$AA,$9B,$AB,$60
 	.byte $10,$01,$F1,$FF,$07,$00,$28,$00
@@ -4752,13 +4772,13 @@ DoWarp:
 	.byte $FF,$3C,$23,$08,$20,$38,$30,$30
 	.byte $30,$0D,$35,$31,$30,$20,$21,$2C
 	.byte $4D,$31,$39,$47,$3F,$58,$57,$80
-vsync:
-//$2E00	
+vsync:			//$2E00	uint8_t vsync count -> irq_hook()
 	.byte $30
-rotatec:
+rotatec:		//uint8_t  index into RotColour
 	.byte $00
-RotColour:
-	.byte $01,$03,$04//Red,Yellow,Blue
+RotColour:		//colour_t  PAL_ROTx colours
+	.byte $01,$03,$04
+//		Red,Yellow,Blue
 AiVector:		//vector table for ai routines
 //$2E05	
 	// .byte $BC,$11,$DA,$11,$61,$13,$F8,$13
@@ -4772,6 +4792,7 @@ AiVector:		//vector table for ai routines
 Spawnc:
 //$2E1D		Duint8_t[8]  Unit spawn counts
 	.byte $00,$05,$00,$00,$04,$00,$00,$00
+//		  SHP LND MUT BAI BOM SWM MAN POD
 XMinInit:
 //$2e25		xpos_t[8]  Initial unit minimum X
 	.byte $07,$00,$40,$00,$40,$00,$00,$00
@@ -4781,6 +4802,7 @@ XRangeInit:
 YMinInit:
 //$2E35		ypos_t[8]  Initial unit minimum Y
 	.byte $64,$B4,$00,$00,$00,$00,$0A,$00
+//		  SHP LND MUT BAI BOM SWM MAN POD	
 YRangeInit:
 //$2E3D		uint8_t[8]  Initial unit Y range
 	.byte $00,$00,$FF,$FF,$FF,$1F,$00,$FF
@@ -4790,6 +4812,7 @@ dXMinInit:
 dXRangeInit:
 //$2E4D		uint8_t[8]  Initial unit dX range
 	.byte $07,$0F,$00,$07,$0F,$07,$00,$07
+//		  SHP LND MUT BAI BOM SWM MAN POD
 dYMinInit:
 //$2E55		yoffset_t[8] Init minimum dY (abs)
 	.byte $0A,$00,$00,$0A,$00,$18,$00,$08
@@ -4811,58 +4834,76 @@ StringV_h:
 	.byte $00,$00,$03,$00,$00,$00,$00,$00
 	.byte $00,$FC,$00,$00,$03
 string0:
-//$2E8D		string_t[3]  Message string #0
+//$2E8D		Message string #0
+//		length,clear text,restore default pallete
 	.byte $02,$0C,$14
 string1:
-//byte 02=length.clear text and restore def pallete	
-//$2E90		string_t[25]  Message string #1
+//$2E90		Message string #1
+//		length,txt clr,PAL_FLASH,cursor,(4,12)
 	.byte $18,$11,$04,$1F,$04,$0C,$E0,$E1
 	.byte $E2,$E3,$E4,$E5,$20,$E6,$E7,$E8
-	.byte $E9,$EA,$1F,$07,$0F,$EB,$EC,$ED
-	.byte $EE
+	.byte $E9,$EA
+//		cursor,(7,15)
+	.byte $1F,$07,$0F,$EB,$EC,$ED,$EE
 string2:
-//$2EA9		string_t[12]  Message string #2
+//$2EA9		Message string #2
+//		length,cursor,(7,15),txt clr #4
 	.byte $0B,$1F,$07,$0F,$11,$04,$EF,$F0
 	.byte $20,$20,$F1,$F2
 string3:
-//$2EB5		string_t[64]  Message string #3
+//$2EB5		Message string #3
 //Planetoid Hall of Fame - double height (mode7)
-	.byte $3F,$16,$07,$81,$9D,$83,$8D,$1F
-	.byte $09,$00,$50,$6C,$61,$6E,$65,$74
-	.byte $6F,$69,$64,$20,$48,$61,$6C,$6C
-	.byte $20,$6F,$66,$20,$46,$61,$6D,$65
-	.byte $1F,$00,$01,$81,$9D,$83,$8D,$1F
-	.byte $09,$01,$50,$6C,$61,$6E,$65,$74
-	.byte $6F,$69,$64,$20,$48,$61,$6C,$6C
-	.byte $20,$6F,$66,$20,$46,$61,$6D,$65
+//		  length,mode,7,double height
+	.byte $3F,$16,$07,$81,$9D,$83,$8D
+	.byte $1F,$09,$00
+//		  cursor,(9,0)
+//		  Planetoid Hall of Fame
+	.byte $50,$6C,$61,$6E,$65,$74,$6F,$69
+	.byte $64,$20,$48,$61,$6C,$6C,$20,$6F
+	.byte $66,$20,$46,$61,$6D,$65
+//		  cursor,(0,1),double height
+	.byte $1F,$00,$01,$81,$9D,$83,$8D
+//		  cursor,(9,1)
+	.byte $1F,$09,$01
+//		  Planetoid Hall of Fame
+	.byte $50,$6C,$61,$6E,$65,$74,$6F,$69
+	.byte $64,$20,$48,$61,$6C,$6C,$20,$6F
+	.byte $66,$20,$46,$61,$6D,$65
 string4:
-//$2EF5		string_t[68]  Message string #4
-//Congratulations - double height
-	.byte $43,$1F,$0B,$03,$86,$8D,$43,$6F
-	.byte $6E,$67,$72,$61,$74,$75,$6C,$61
-	.byte $74,$69,$6F,$6E,$73,$1F,$0B,$04
-	.byte $86,$8D,$43,$6F,$6E,$67,$72,$61
-	.byte $74,$75,$6C,$61,$74,$69,$6F,$6E
-	.byte $73,$1F,$08,$17,$86,$88,$50,$6C
+//$2EF5		Message string #4
+//		  length,cursor,(11,3),double height
+	.byte $43,$1F,$0B,$03,$86,$8D
+//Congratulations
+	.byte $43,$6F,$6E,$67,$72,$61,$74,$75
+	.byte $6C,$61,$74,$69,$6F,$6E,$73
+//		  cursor,(11,4),double height
+	.byte $1F,$0B,$04,$86,$8D
+	.byte $43,$6F,$6E,$67,$72,$61,$74,$75
+	.byte $6C,$61,$74,$69,$6F,$6E,$73
+//		  cursor,(8,23),double height
+	.byte $1F,$08,$17,$86,$88
 //Please enter your name	
-	.byte $65,$61,$73,$65,$20,$65,$6E,$74
-	.byte $65,$72,$20,$79,$6F,$75,$72,$20
-	.byte $6E,$61,$6D,$65
+	.byte $50,$6C,$65,$61,$73,$65,$20,$65
+	.byte $6E,$74,$65,$72,$20,$79,$6F,$75
+	.byte $72,$20,$6E,$61,$6D,$65
 string5:
-//$2F39		string_t[6]  Message string #5
+//$2F39		Message string #5
+// length,' ... '
 	.byte $05,$20,$2E,$2E,$2E,$20
-// '...'
 string6:
-//$2F3F		string_t[81]  Message string #6
+//$2F3F		Message string #6
+//		  length,cursor,(10,3),double height
+	.byte $50,$1F,$0A,$03,$8D,$86
 //Today's Greatest
-	.byte $50,$1F,$0A,$03,$8D,$86,$54,$6F
-	.byte $64,$61,$79,$27,$73,$20,$47,$72
-	.byte $65,$61,$74,$65,$73,$74,$1F,$0A
-	.byte $04,$8D,$86,$54,$6F,$64,$61,$79
-	.byte $27,$73,$20,$47,$72,$65,$61,$74
-	.byte $65,$73,$74,$1F,$02,$17,$86,$88
-//Press the SPACE BAR 
-//to play again
+	.byte $54,$6F,$64,$61,$79,$27,$73,$20
+	.byte $47,$72,$65,$61,$74,$65,$73,$74
+//		  cursor,(10,4),double height
+	.byte $1F,$0A,$04,$8D,$86
+	.byte $54,$6F,$64,$61,$79,$27,$73,$20
+	.byte $47,$72,$65,$61,$74,$65,$73,$74
+//		  cursor,(2,23),flashing
+	.byte $1F,$02,$17,$86,$88
+//Press the SPACE BAR to play again
 	.byte $50,$72,$65,$73,$73,$20,$74,$68
 	.byte $65,$20,$53,$50,$41,$43,$45,$20
 	.byte $42,$41,$52,$20,$74,$6F,$20,$70
